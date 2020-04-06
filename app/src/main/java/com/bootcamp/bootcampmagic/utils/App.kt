b7ab9178301694c8a0b5d1a685716f957bbe0e6b
ruiderson.o.andrade <ruiderson.o.andrade@accenture.com>
2020-04-06 10:25:34 -0300

    Change Retrofit url path to Application class

--- app/src/main/java/com/bootcamp/bootcampmagic/utils/App.kt
+++ app/src/main/java/com/bootcamp/bootcampmagic/utils/App.kt
@@ -2,12 +2,15 @@ package com.bootcamp.bootcampmagic.utils
 
 import android.app.Application
 import androidx.room.Room
+import com.bootcamp.bootcampmagic.BuildConfig
 import com.bootcamp.bootcampmagic.repositories.CardsDao
+import com.bootcamp.bootcampmagic.repositories.CardsDataSource
 import com.bootcamp.bootcampmagic.repositories.CardsDatabase
 
 class App: Application() {
 
     private lateinit var cardsDatabase: CardsDatabase
+    private lateinit var cardsDataSource: CardsDataSource
 
     override fun onCreate() {
         super.onCreate()
@@ -17,11 +20,12 @@ class App: Application() {
             CardsDatabase::class.java,
             CardsDatabase.DATABASE_NAME)
             .build()
+
+        cardsDataSource = RetrofitInitializer.getCardsDataSource(BuildConfig.API_BASE_URL)
     }
 
 
-    fun getCardsDao(): CardsDao{
-        return cardsDatabase.getCardsDao()
-    }
+    fun getCardsDao(): CardsDao = cardsDatabase.getCardsDao()
+    fun getCardsDataSource(): CardsDataSource = cardsDataSource
 
 }
--- app/src/main/java/com/bootcamp/bootcampmagic/utils/RetrofitInitializer.kt
+++ app/src/main/java/com/bootcamp/bootcampmagic/utils/RetrofitInitializer.kt
@@ -1,15 +1,11 @@
 package com.bootcamp.bootcampmagic.utils
 
-import com.bootcamp.bootcampmagic.BuildConfig
 import com.bootcamp.bootcampmagic.repositories.CardsDataSource
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInitializer {
 
-    fun getCardsDataSource(): CardsDataSource {
-        return getCardsDataSource(BuildConfig.API_BASE_URL)
-    }
     fun getCardsDataSource(url: String): CardsDataSource {
         return Retrofit.Builder()
             .baseUrl(url)
