d594f81dd82383a6c1018a4e64b79bb6767cc2ef
ruiderson.o.andrade <ruiderson.o.andrade@accenture.com>
2020-04-07 16:09:47 -0300

    Fixed EndlessScrollListener

--- app/src/main/java/com/bootcamp/bootcampmagic/adapter/EndlessScrollListener.kt
+++ app/src/main/java/com/bootcamp/bootcampmagic/adapter/EndlessScrollListener.kt
@@ -3,81 +3,86 @@ package com.bootcamp.bootcampmagic.adapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+
+abstract class EndlessScrollListener(
+    private val recyclerView: RecyclerView
+) : RecyclerView.OnScrollListener() {
+
+    private val layoutManager: RecyclerView.LayoutManager = recyclerView.layoutManager!!
+    private val percentage = 50
+    private var currentPosition: Position = Position.START
+
+    abstract fun onFirstItem()
+    abstract fun onScroll()
+    abstract fun onLoadMore()
 
 
-abstract class EndlessScrollListener : RecyclerView.OnScrollListener {
+    init{
+        this.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
 
-    private var visibleThreshold = 5
-    private var currentPage = 1
-    private var previousTotalItemCount = 0
-    private var loading = true
-    private val startingPageIndex = 0
-    var mLayoutManager: RecyclerView.LayoutManager
 
-    constructor(layoutManager: LinearLayoutManager) {
-        mLayoutManager = layoutManager
-    }
+                //Check if reached the first item
+                val firstItem = when(layoutManager){
+                    is LinearLayoutManager ->
+                        layoutManager.findFirstCompletelyVisibleItemPosition()
+                    is GridLayoutManager ->
+                        layoutManager.findFirstCompletelyVisibleItemPosition()
+                    else -> 0
+                }
 
-    constructor(layoutManager: GridLayoutManager) {
-        mLayoutManager = layoutManager
-        visibleThreshold *= layoutManager.spanCount
-    }
+                if(firstItem == 0){
+                    if(currentPosition != Position.START){
+                        onFirstItem()
+                    }
+                    currentPosition = Position.START
+                    return
+                }
 
-    constructor(layoutManager: StaggeredGridLayoutManager) {
-        mLayoutManager = layoutManager
-        visibleThreshold *= layoutManager.spanCount
-    }
 
-    fun getLastVisibleItem(lastVisibleItemPositions: IntArray): Int {
-        var maxSize = 0
-        for (i in lastVisibleItemPositions.indices) {
-            if (i == 0) {
-                maxSize = lastVisibleItemPositions[i]
-            } else if (lastVisibleItemPositions[i] > maxSize) {
-                maxSize = lastVisibleItemPositions[i]
+                //Check if reached the load more position
+                val totalItemCount = recyclerView.layoutManager!!.itemCount
+                val lastItem = when(layoutManager){
+                    is LinearLayoutManager ->
+                        layoutManager.findLastVisibleItemPosition()
+                    is GridLayoutManager ->
+                        layoutManager.findLastVisibleItemPosition()
+                    else -> 0
+                }
+
+                val currentPercentage = ((totalItemCount * percentage) / 100)
+                if(lastItem >= currentPercentage){
+                    if(currentPosition != Position.LOAD_MORE){
+                        onLoadMore()
+                    }
+
+                    currentPosition = Position.LOAD_MORE
+                    return
+                }
+
+
+                //Scrolling
+                if(currentPosition != Position.LOAD_MORE){
+                    onScroll()
+                }
+                currentPosition = Position.SCROLLING
+
+
             }
-        }
-        return maxSize
+        })
+    }
+
+
+    fun reset(){
+        currentPosition = Position.SCROLLING
     }
 
 
 
-    override fun onScrolled(view: RecyclerView, dx: Int, dy: Int) {
-        var lastVisibleItemPosition = 0
-        val totalItemCount = mLayoutManager.itemCount
-        if (mLayoutManager is StaggeredGridLayoutManager) {
-            val lastVisibleItemPositions =
-                (mLayoutManager as StaggeredGridLayoutManager).findLastVisibleItemPositions(null)
-            // get maximum element within the list
-            lastVisibleItemPosition = getLastVisibleItem(lastVisibleItemPositions)
-        } else if (mLayoutManager is LinearLayoutManager) {
-            lastVisibleItemPosition =
-                (mLayoutManager as LinearLayoutManager).findLastVisibleItemPosition()
-        } else if (mLayoutManager is GridLayoutManager) {
-            lastVisibleItemPosition =
-                (mLayoutManager as GridLayoutManager).findLastVisibleItemPosition()
-        }
-
-
-        if (totalItemCount < previousTotalItemCount) {
-            currentPage = startingPageIndex
-            previousTotalItemCount = totalItemCount
-            if (totalItemCount == 0) {
-                loading = true
-            }
-        }
-        if (loading && totalItemCount > previousTotalItemCount) {
-            loading = false
-            previousTotalItemCount = totalItemCount
-        }
-
-        if (!loading && lastVisibleItemPosition + visibleThreshold > totalItemCount) {
-            currentPage++
-            onLoadMore(currentPage, totalItemCount)
-            loading = true
-        }
+    private enum class Position(val value: Int) {
+        START(1),
+        SCROLLING(2),
+        LOAD_MORE(3)
     }
-
-    abstract fun onLoadMore(page: Int, totalItemsCount: Int)
 }
--- app/src/main/java/com/bootcamp/bootcampmagic/ui/SetsFragment.kt
+++ app/src/main/java/com/bootcamp/bootcampmagic/ui/SetsFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bootcamp.bootcampmagic.R
 import com.bootcamp.bootcampmagic.adapter.AdapterCards
 import com.bootcamp.bootcampmagic.adapter.EndlessScrollListener
@@ -20,18 +17,19 @@ import com.bootcamp.bootcampmagic.repositories.CardsRepository
 import com.bootcamp.bootcampmagic.utils.App
 import com.bootcamp.bootcampmagic.viewmodels.SetsViewModel
 import com.bootcamp.bootcampmagic.viewmodels.SetsViewModelFactory
+import com.bootcamp.bootcampmagic.viewmodels.SetsViewModelState
 import kotlinx.android.synthetic.main.fragment_set.*
 
 class SetsFragment() : Fragment() {
 
+    private lateinit var adapterCards: AdapterCards
+    private var isRefreshing = true
     private val viewModel: SetsViewModel by viewModels{
         App().let {
             SetsViewModelFactory(CardsRepository(it.getCardsDataSource(), it.getCardsDao()))
         }
     }
 
-    private lateinit var adapterCards: AdapterCards
-    private val listCards = mutableListOf<Card>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,49 +42,67 @@ class SetsFragment() : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        recycler_cards.visibility = View.GONE
-
+        //recycler_cards.visibility = View.GONE
+        setupRecyclerView()
         setupObservables()
-        setupRecyclerView(viewModel.getData().value!!)
     }
 
 
-    private fun setupObservables(){
-        viewModel.getViewState().observe(viewLifecycleOwner, Observer {
-            showErrorMessage(it.toString())
-            progress_circular.visibility = View.GONE
-        })
+    private fun setupRecyclerView(){
 
-        viewModel.getData().observe(viewLifecycleOwner, Observer {
-            updateRecyclerView(it)
-        })
-    }
-
-    private fun setupRecyclerView(listCards: List<Card>){
-        this.listCards.addAll(listCards)
-
-        adapterCards = AdapterCards(listCards)
-        val gridLayoutManager = GridLayoutManager(context, 3)
-
-        recycler_cards.layoutManager = gridLayoutManager
+        adapterCards = AdapterCards(clickListener)
+        recycler_cards.layoutManager = GridLayoutManager(context, 3)
         recycler_cards.adapter = adapterCards
 
-        recycler_cards.visibility = View.VISIBLE
-        progress_circular.visibility = View.GONE
-
-        val endlessScroll = object: EndlessScrollListener(gridLayoutManager){
-            override fun onLoadMore(page: Int, totalItemsCount: Int) {
+        val endlessScrollListener = object : EndlessScrollListener(recycler_cards){
+            override fun onFirstItem() {
+            }
+            override fun onScroll() {
+            }
+            override fun onLoadMore() {
                 viewModel.loadCards()
             }
         }
+
     }
 
-    private fun updateRecyclerView(listCards: List<Card>){
-        this.listCards.addAll(listCards)
-        adapterCards.notifyDataSetChanged()
+    private val clickListener = object: AdapterCards.OnItemClickListener{
+        override fun onItemClicked(card: Card, position: Int) {
+        }
+    }
+
+    private fun setupObservables(){
+        viewModel.getViewState().observe(viewLifecycleOwner, Observer {
+            when(it){
+
+                is SetsViewModelState.Error ->
+                    showErrorMessage(it.toString())
+
+                is SetsViewModelState.CacheLoaded -> {
+                    progress_circular.visibility = View.GONE
+                    refresh()
+                }
+
+            }
+        })
+        viewModel.getData().observe(viewLifecycleOwner, Observer {
+            if(it.isNotEmpty()){
+                when(isRefreshing){
+                    true -> adapterCards.setItems(it)
+                    else -> adapterCards.addItems(it)
+                }
+                isRefreshing = false
+            }
+        })
+    }
+
+    private fun refresh(){
+        isRefreshing = true
+        viewModel.refresh()
     }
 
     private fun showErrorMessage(errorMessage: String){
+        progress_circular.visibility = View.GONE
         Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
     }
 }
