392c3034adb87f0ee8ca4cd56b5ba61426696bab
Guilherme <guisil_ps@hotmail.com>
2020-04-07 16:56:00 -0300

    Setting up intrumented tests for main and sets

--- app/src/androidTest/java/com/bootcamp/bootcampmagic/MainActivityTest.kt
+++ app/src/androidTest/java/com/bootcamp/bootcampmagic/MainActivityTest.kt
@@ -1,25 +1,28 @@
 package com.bootcamp.bootcampmagic
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.bootcamp.bootcampmagic.ui.MainActivity
+import com.google.android.material.tabs.TabLayout
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-    
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bootcamp.bootcampmagic", appContext.packageName)
+    fun givenInitialState_shouldDisplayTabLayout(){
+        Espresso.onView(ViewMatchers.withId(R.id.tab_set_favorites))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
